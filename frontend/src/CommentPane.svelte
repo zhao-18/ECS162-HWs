d3f8f8302692d94ea080c3167db957e8fd4a6a94
zhao-18 <zhao1455@gmail.com>
2025-05-15 14:25:23 -0700

    add components for comment

---
+++ frontend/src/CommentPane.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Comment from "./lib/Comment.svelte";
+
+    let {visibility} = $props();
+</script>
+
+<div id="commentPane">
+    <input type="checkbox" id="invokeCommentPane" bind:checked={visibility.state}/>
+    <div id="overlay" onclick={() => visibility.state = !visibility.state}></div>
+    <div id="comments">
+        <Comment username="trebor" locale="USA" date="May 15" content="YEHEHAHHAHA" recommend={22} replyNum={14} />
+        <Comment username="trebor" locale="USA" date="May 15" content="YEHEHAHHAHA" recommend={22} replyNum={14} />
+        <Comment username="trebor" locale="USA" date="May 15" content="YEHEHAHHAHA" recommend={22} replyNum={14} />
+        <Comment username="trebor" locale="USA" date="May 15" content="YEHEHAHHAHA" recommend={22} replyNum={14} />
+        <Comment username="trebor" locale="USA" date="May 15" content="YEHEHAHHAHA" recommend={22} replyNum={14} />
+        <Comment username="trebor" locale="USA" date="May 15" content="YEHEHAHHAHA" recommend={22} replyNum={14} />
+        <Comment username="trebor" locale="USA" date="May 15" content="YEHEHAHHAHA" recommend={22} replyNum={14} />
+        <Comment username="trebor" locale="USA" date="May 15" content="YEHEHAHHAHA" recommend={22} replyNum={14} />
+        <Comment username="trebor" locale="USA" date="May 15" content="YEHEHAHHAHA" recommend={22} replyNum={14} />
+    </div>
+</div>
---
+++ frontend/src/lib/Comment.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    interface comment {
+        username: string;
+        locale: string;
+        date: string;
+        content: string;
+        recommend: number;
+        replyNum: number;
+    }
+    let props: comment = $props();
+</script>
+
+<div id="comment">
+    <span id="username">{props.username}</span>
+    <div id="metadata">
+        <span id="locale">{props.locale}</span>
+        <div class="vline-light"></div>
+        <span id="date">{props.date}</span>
+    </div>
+    <p id="content">{props.content}</p>
+    <div id="social">
+        <button id="reply">Reply</button>
+        <button id="recommend"><span>{props.recommend}</span> Recommend</button>
+        <button id="share">Share</button>
+    </div>
+    <span id="num-reply">{props.replyNum} REPLIES</span>
+</div>
