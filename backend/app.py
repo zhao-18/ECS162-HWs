73e3cc44a78459aac3bfb8c8836c298e9cbebacb
Navjeet Hundal <navjeethundal03@gmail.com>
2025-05-17 03:35:41 -0700

    Added comment, article, and moderation routes with MongoDB integration

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,8 @@ import os
 from flask_cors import CORS
 import requests
 from pymongo import MongoClient
+from bson.objectid import ObjectId
+from datetime import datetime, timezone
 
 static_path = os.getenv('STATIC_PATH','static')
 template_path = os.getenv('TEMPLATE_PATH','templates')
@@ -41,6 +43,104 @@ def serve_frontend(path=''):
 @app.route('/login')
 def login_frontend():
     return send_from_directory(template_path, 'login.html')
+
+# This code will end up getting all comments for the article. 
+# In order to understand MongoDB better we read up on the documentation given during lab.
+# MongoDB in a Flask Application: https://www.digitalocean.com/community/tutorials/how-to-use-mongodb-in-a-flask-application 
+# Sending Data from a Flask app to MongoDB Database: https://www.geeksforgeeks.org/sending-data-from-a-flask-app-to-mongodb-database/ 
+
+@app.route("/api/comments", methods=["GET"])
+def get_comments():
+    article_id = request.args.get("article_id")
+    comments = list(db.comments.find({"article_id": article_id}))
+    for i in comments:
+        i["_id"] = str(i["_id"])
+    return jsonify(comments)
+
+# This code will allow the frontend to get a saved article from the MongoDB. This will be helpful for linking comments to articles. 
+# In order to understand MongoDB better we read up on the documentation given during lab.
+# MongoDB in a Flask Application: https://www.digitalocean.com/community/tutorials/how-to-use-mongodb-in-a-flask-application 
+# Sending Data from a Flask app to MongoDB Database: https://www.geeksforgeeks.org/sending-data-from-a-flask-app-to-mongodb-database/ 
+
+@app.route("/api/articles/<article_id>", methods=["GET"])
+def get_article(article_id):
+    article = db.articles.find_one({"_id": article_id})
+    if article:
+        article["_id"] = str(article["_id"])
+        return jsonify(article)
+
+# This code will allow a new comment or a new reply to the database. 
+# The database will have the id of the article, content of the comment, email, timestamp, and moderated comment.
+# In order to understand MongoDB better we read up on the documentation given during lab.
+# MongoDB in a Flask Application: https://www.digitalocean.com/community/tutorials/how-to-use-mongodb-in-a-flask-application 
+# Sending Data from a Flask app to MongoDB Database: https://www.geeksforgeeks.org/sending-data-from-a-flask-app-to-mongodb-database/  
+
+@app.route("/api/comments", methods=["POST"])
+def post_comment():
+    data = request.get_json()
+    comment = {
+        "article_id": data["article_id"],
+        "user": request.user["email"],
+        "text": data["text"],
+        "parent_id": data.get("parent_id", "root"),
+        "created_at": datetime.now(timezone.utc),
+        "moderated": False
+    }
+    result = db.comments.insert_one(comment)
+    comment["_id"] = str(result.inserted_id)
+    return jsonify(comment), 201
+
+# This code will allow the moderater to delete a comment which should only be accessible by the moderators. 
+# The code works by checking if the current user is a moderator, then updates the comment and returns message.
+# In order to understand MongoDB better we read up on the documentation given during lab.
+# MongoDB in a Flask Application: https://www.digitalocean.com/community/tutorials/how-to-use-mongodb-in-a-flask-application 
+# Sending Data from a Flask app to MongoDB Database: https://www.geeksforgeeks.org/sending-data-from-a-flask-app-to-mongodb-database/ 
+
+@app.route("/api/comments/<comment_id>/moderater", methods=["POST"])
+def moderater_comment(comment_id):
+    if not request.user["is_moderator"]:
+        return jsonify({"error": "Unauthorized/Not Moderator"}), 400
+    
+    db.comments.update_one(
+        {"_id": ObjectId(comment_id)},
+        {"$set": {"text": "COMMENT REMOVED BY MODERATOR!", "moderated": True}}
+    )
+    return jsonify({"message": "Comment deleted"}), 200
+
+# The code will save an article to MongoDB. 
+# The code works by checking if there is an article url then builds the main componenets that we want to store in the database such as id, title, summary, url, author, and timestamp.
+# In order to understand MongoDB better we read up on the documentation given during lab.
+# MongoDB in a Flask Application: https://www.digitalocean.com/community/tutorials/how-to-use-mongodb-in-a-flask-application 
+# Sending Data from a Flask app to MongoDB Database: https://www.geeksforgeeks.org/sending-data-from-a-flask-app-to-mongodb-database/ 
+
+@app.route("/api/articles", methods=["POST"])
+def save_article():
+    data = request.get_json()
+    if not data or "url" not in data:
+        return jsonify({"error": "Missing article URL"}), 400
+
+    article = {
+        "_id": data.get("_id") or data["url"],
+        "headline": data.get("headline"),
+        "abstract": data.get("abstract"),
+        "url": data["url"],
+        "byline": data.get("byline"),
+        "stored_at": datetime.now(timezone.utc)
+    }
+
+    db.articles.replace_one({"_id": article["_id"]}, article, upsert=True)
+    return jsonify({"message": "Article saved"}), 200
+
+
+# -------- Things to do still ------------
+
+# Add extra credit moderator redacted (if time) (Add as a if else statement in moderator route)
+
+# Add Dex authication and then clean up / simplify some MongoDB code (read up on dex documentation)
+
+# Add some more backend unit testing in app_test.py
+
+# ---------------------------------------
 
 @app.route("/test-mongo")
 def test_mongo():
